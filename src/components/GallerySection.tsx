c7595abe01db8d679f403187c9572cc1c0e04859
luciafemia23-debug <luciafemia23@gmail.com>
2026-05-10 19:40:47 +0200

    .

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -11,7 +11,7 @@ import gallery3 from "@/assets/gallery-3.jpg";
 import gallery4 from "@/assets/gallery-4.jpg";
 import gallery5 from "@/assets/gallery-5.jpg";
 import gallery6 from "@/assets/gallery-6.jpg";
-
+import gallery1 from "@/assets/gallery-1.jpg";
 
 const galleryItems = [
   { src: gallery2 },
